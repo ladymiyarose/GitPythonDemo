7232401334ff4189fedebdfcff1bb41458ef7936
Sharon Hall <perkins@uhcl.edu>
2018-10-19 15:24:46 -0500

    drawing legs

--- legs_drawer.py
+++ legs_drawer.py
@@ -1,3 +1,12 @@
 def draw_legs(pen):
-    pen.forward(50)
+    pen.penup()
+    pen.setposition(100, 100)
+    pen.pendown()
+    pen.right(100)
+    pen.forward(100)
+    pen.penup()
+    pen.setposition(100, 120)
+    pen.pendown()
+    pen.right(100)
+    pen.forward(100)
     return
